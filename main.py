1aca659d5c8c18ae7b038f39a4bdee9c6a37f984
Mehnaz Shaikh <24f1002437@ds.study.iitm.ac.in>
2025-12-12 20:51:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def healthz():
         "uptime_seconds": int(time.time() - START_TIME)
     }
 
-@app.post("/quiz")
+@app.post("/solve")
 async def solve(request: Request, background_tasks: BackgroundTasks):
     try:
         data = await request.json()
